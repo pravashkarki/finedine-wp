65580a2412e307e5ee79966dd376dfc1a6f8fdc8
Pravash Karki <pravash@lastdoorsolutions.com>
2020-04-09 16:42:06 +0545

    Remove demo content file from theme

--- demo/demo.php
+++ demo/demo.php
@@ -12,14 +12,6 @@ $config = array(
 	'menu_locations' => array(
 		'menu-1' => 'main-menu',
 	),
-	'ocdi'           => array(
-		array(
-			'import_file_name'             => esc_html__( 'Theme Demo Content', 'finedine' ),
-			'local_import_file'            => trailingslashit( get_template_directory() ) . 'demo/demo-content/content.xml',
-			'local_import_widget_file'     => trailingslashit( get_template_directory() ) . 'demo/demo-content/finedine-widget.wie',
-			'local_import_customizer_file' => trailingslashit( get_template_directory() ) . 'demo/demo-content/finedine-export.dat',
-		),
-	),
 );
 
 Finedine_Demo_Import::init( apply_filters( 'Finedine_Demo_Import_filter', $config ) );
