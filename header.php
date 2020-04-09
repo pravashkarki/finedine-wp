121321720c31dec51df3e8ebe587d29b89460a0c
Pravash Karki <pravash@lastdoorsolutions.com>
2020-04-09 16:10:09 +0545

    Add body open hook

--- header.php
+++ header.php
@@ -17,9 +17,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
-</head>    
+</head>
 
 <body <?php body_class(); ?> >
+<?php do_action( 'wp_body_open' ); ?>
+
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'finedine' ); ?></a>
 
